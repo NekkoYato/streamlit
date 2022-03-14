853239d7c54db2697b6f65e56967e00965523231
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-15 01:26:21 +0530

    kanna-chan!

---
+++ kanna.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import requests
+from PIL import Image
+from validators.url import url
+st.title('Kanna Says!!')
+text = st.text_input("What do you want kanna to say?\n")
+r = requests.get(
+    f"https://nekobot.xyz/api/imagegen?type=kannagen&text={text}"
+    ).json()
+a = r.get("message")
+iurl = url(a)
+with open("temp.png", "wb") as f:
+    f.write(requests.get(a).content)
+img = Image.open("temp.png").convert("RGB")
+st.image(img)
